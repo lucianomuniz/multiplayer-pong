b8e96f1fe867b339b6f5f188d9b9a5b943aac13f
Luciano Muniz <luciano.mo@gmail.com>
2022-04-14 16:36:32 -0300

    Add socket room

--- sockets.js
+++ sockets.js
@@ -3,31 +3,37 @@ let readyPlayerCount = 0;
 function listen(io) {
   const pongNamespace = io.of("/pong");
   pongNamespace.on("connection", (socket) => {
+    let room;
+
     console.log("a user connected", socket.id);
 
     socket.on("ready", () => {
-      console.log("`Player ready", socket.id);
+      room = "room" + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+
+      console.log("Player ready", socket.id, room);
 
       readyPlayerCount++;
 
       // broadcast message to all players to startGame
       if (readyPlayerCount % 2 === 0) {
-        pongNamespace.emit("startGame", socket.id);
+        pongNamespace.in(room).emit("startGame", socket.id);
       }
     });
 
-    // broadcast to all players except to the sender
+    // broadcast to all players in the room except to the sender
     socket.on("paddleMove", (paddleData) => {
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(room).emit("paddleMove", paddleData);
     });
 
-    // broadcast to all players except to the sender
+    // broadcast to all players in the room except to the sender
     socket.on("ballMove", (ballData) => {
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(room).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
-      console.log(`Client ${socket.id} disconnected: ${reason}`);
+      console.log(`Client ${socket.id} disconnected from ${room}: ${reason}`);
+      socket.leave(room);
     });
   });
 }
